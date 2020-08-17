4ebf356d3994020ce1ee8eef4b876906ec6f4bfa
= <=>
2020-08-17 13:03:55 +0700

    create js class tile

---
+++ src/static/js/tile.js
@@ -0,0 +1,42 @@
+
+tileHTML = '<div id="reveal-tile" class="reveal-tile">\n' +
+    '    <div class="reveal-tile-bar">\n' +
+    '        <button id="tile-roll-down-btn">\n' +
+    '            <i class="fas fa-minus"></i>\n' +
+    '        </button>\n' +
+    '        <button id="tile-close-btn">\n' +
+    '            <i class="fas fa-times"></i>\n' +
+    '        </button>\n' +
+    '    </div>\n' +
+    '\n' +
+    '    <div class="reveal-tile-container">\n' +
+    '\n' +
+    '        <div class="reveal-tile-author-container">\n' +
+    '\n' +
+    '            <div class="reveal-tile-author-img-container">\n' +
+    '                <img src="{{ url_for(\'static\', filename=\'img/user-3.jpg\') }}" alt="" class="reveal-tile-author-img">\n' +
+    '            </div>\n' +
+    '\n' +
+    '            <h2 class="reveal-tile-author"><span>Some User</span></h2>\n' +
+    '\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="reveal-tile-content">\n' +
+    '            <div class="reveal-tile-content-element">\n' +
+    '                <span class="reveal-tile-text">\n' +
+    '                    Lorem ipsum dolor sit amet, consectetur adipisicing elit At atque beatae\n' +
+    '                commodi consequuntur cupiditate dicta dolor illum iure\n' +
+    '                sequi voluptates. Ab accusamus aperiam blanditiis corporis\n' +
+    '                inventore minus odio quas quibusdam.\n' +
+    '                </span>\n' +
+    '            </div>\n' +
+    '            <div class="reveal-tile-content-element">\n' +
+    '                <div class="reveal-tile-content-img-container">\n' +
+    '                    <img src="{{ url_for(\'static\', filename=\'img/user-2.jpg\') }}" alt="" class="reveal-tile-content-img">\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '\n' +
+    '    </div>\n' +
+    '\n' +
+    '</div>'
--- src/static/js/wall.js
+++ src/static/js/wall.js
@@ -1,6 +1,72 @@
+// import(tileHTML)
+
 class Wall {
     main = document.getElementsByTagName('main')[1];
     wall = document.getElementById('wall');
 }
 
+class Tile {
+    element = document.createElement('li');
+    header = document.createElement('header');
+    tileAuthorImgContainer = document.createElement('div');
+    tileAuthorImg = document.createElement('img');
+    tileAuthor = document.createElement('h3');
+    tileContent = document.createElement('div');
 
+    id = NaN;
+    type = NaN;
+    contentElements = [];
+    author = {
+        username: NaN,
+        imagePath: NaN,
+    };
+
+    createTile() {
+
+        this.element.className = 'tile ';
+        this.header.className = 'tile_header';
+        this.tileAuthorImgContainer.className = 'tile_author_img_container';
+        this.tileAuthorImg.className = 'tile_author_img';
+        this.tileAuthor.className='tile_author';
+        this.tileContent.className='tile_content';
+
+        for (let contentElement in this.contentElements) {
+
+        }
+
+        this.element.appendChild(this.header);
+        this.tileAuthorImgContainer.appendChild(this.tileAuthorImg)
+        this.header.appendChild(this.tileAuthorImgContainer);
+        this.header.appendChild(this.tileAuthor);
+
+    }
+
+    constructor(id, type, contentElements, username, imagePath) {
+        this.id = id;
+        this.type = type;
+        this.contentElements = contentElements;
+        this.author.username = username;
+        this.author.imagePath = imagePath;
+
+        if (this.type) {
+            this.element.className += this.type;
+        }
+    }
+
+    draw() {
+        let wall = document.getElementById('wall');
+        wall.appendChild(this.element);
+    }
+
+    reveal() {
+        let main = document.getElementsByTagName('main')[0]
+        let revealTile = document.createElement('div')
+
+        revealTile.className = 'reveal-tile'
+    }
+
+}
+
+let tile = new Tile(1, 'large-tile')
+
+tile.draw()
