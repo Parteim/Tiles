dd74c834f5a0f615a085426bc0c7be1640be914b
= <=>
2020-08-25 06:46:14 +0700

    profile user update

--- src/static/js/profile.js
+++ src/static/js/profile.js
@@ -1,36 +1,34 @@
+// import Request from 'requests.js';
 
 function imageMenu() {
-    let profileImageMenu = document.getElementById('profile_image_menu');
-    let openMenuBtn = document.getElementById('open_menu');
-    let arrowBtn = document.getElementById('arrow_btn');
-    let flag = false;
 
     let callUploadImgForm = document.getElementById('call_upload_img_form');
+    let uploadImgForm = document.createElement('form');
+    let imageField = document.createElement('input');
+    let imagedLabel = document.createElement('label');
+    let closeBtnUploadImageForm = document.createElement('button');
 
-    openMenuBtn.addEventListener('click', function () {
-
-        if (!flag) {
-            profileImageMenu.style.transform = 'translate(-50%, 0%)';
-            arrowBtn.style.transform = 'rotate(180deg)'
-            setTimeout(function (){
-                profileImageMenu.style.transform = 'translate(0%, 0%)';
-            }, 600)
-            flag = true;
-        } else {
-            profileImageMenu.style.transform = 'translate(-50%, 0)';
-            arrowBtn.style.transform = 'rotate(0deg)'
-            setTimeout(function () {
-                profileImageMenu.style.transform = 'translate(-50%, -90%)';
-            }, 600)
-            flag = false;
-        }
-    });
+    uploadImgForm.className = 'upload_img_form';
+    uploadImgForm.appendChild(imagedLabel);
+    uploadImgForm.appendChild(closeBtnUploadImageForm);
+    imageField.accept = 'image/*';
+    imageField.name = 'image';
+    imageField.type = 'file';
+    imageField.className = 'upload_image_field';
+    imagedLabel.draggable = true;
+    imagedLabel.className = 'upload_image_label';
+    imagedLabel.innerHTML = '<i class="fas fa-file-upload"></i>';
+    imagedLabel.appendChild(imageField);
+    closeBtnUploadImageForm.className = 'close_upload_image_form_bnt';
+    closeBtnUploadImageForm.innerHTML = '<i class="fas fa-times"></i>';
 
     callUploadImgForm.addEventListener('click', function () {
-       let uploadImgForm = document.createElement('form');
-       uploadImgForm.className = 'upload_img_form';
+        document.getElementsByTagName('body')[0].appendChild(uploadImgForm);
 
-       document.getElementsByTagName('body')[0].appendChild(uploadImgForm)
+        setTimeout(function () {
+            uploadImgForm.style.height = ' 20%';
+        }, 400)
+
     });
 
 
